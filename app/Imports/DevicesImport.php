bd4e480dc073b58796525fabbd0e321a6682931b
@alaa <alaa@scu.eg>
2025-11-02 15:35:11 +0200

    edit devise import

--- app/Imports/DevicesImport.php
+++ app/Imports/DevicesImport.php
@@ -10,26 +10,22 @@ use App\Models\services;
 use App\Models\UniDevices;
 use App\Models\UniLabs;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Row;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\BeforeImport;
-//use phpDocumentor\Reflection\Types\Collection;
-use Illuminate\Support\Collection;
-
 
 class DevicesImport implements ToCollection, WithHeadingRow, SkipsEmptyRows, WithEvents
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-
     private $hasRows = false;
+    private $fac_id;
+
+    public function __construct($fac_id)
+    {
+        $this->fac_id = $fac_id;
+    }
 
     public function registerEvents(): array
     {
@@ -37,7 +33,7 @@ class DevicesImport implements ToCollection, WithHeadingRow, SkipsEmptyRows, Wit
             BeforeImport::class => function (BeforeImport $event) {
                 $sheet = $event->reader->getActiveSheet();
                 $highestRow = $sheet->getHighestRow();
-                
+
                 if ($highestRow <= 1) {
                     throw new \Exception('The file contains no rows to import.');
                 }
@@ -47,108 +43,136 @@ class DevicesImport implements ToCollection, WithHeadingRow, SkipsEmptyRows, Wit
         ];
     }
 
-    public function  __construct($fac_id)
-    {
-        $this->fac_id= $fac_id;
-    }
-
     public function collection(Collection $rows)
     {
-        if(!$this->hasRows){
+        if (!$this->hasRows) {
             return null;
         }
-        try{
-            if ( $this->fac_id!='central') {
-                if (Auth()->user()->hasRole('university')) {
-                    $dept_ids = dept_fac::where('uni_id', Auth()->user()->uni_id)->where('fac_id', $this->fac_id)
-                        ->join('departments', 'dept_fac.dept_id', '=', 'departments.id')->pluck('departments.id', 'departments.name');
-                }
-                else{
-                    $dept_ids = dept_fac::where('uni_id', Auth()->user()->uni_id)->where('fac_id', Auth()->user()->fac_id)
-                        ->join('departments', 'dept_fac.dept_id', '=', 'departments.id')->pluck('departments.id', 'departments.name');
-                }
-            }
-            foreach ($rows as $row)
-            {
-                if ($row->filter()->isNotEmpty()) {
-                    $device = $this->fac_id == 'central' ?
-                        UniDevices::create([
-                            'name' => $row['name'],
-                            'Arabicname' => $row['arabic_name'],
-                            'ImagePath' => 'images/universities/' . auth()->user()->uni_id . '/' . $row['image_name_with_extension'],
-                            'model' => $row['model'],
-                            'manufacturer' => $row['manufacturer'],
-                            'lab_id' => UniLabs::where('name', $row['lab_name'])->where('uni_id', auth()->user()->uni_id)->pluck('id')->first(),
-                            'uni_id' => auth()->user()->uni_id,
-                            'num_units' => $row['num_units'],
-                            'services' => $row['services_separate_with_comma'],
-                            'servicesArabic' => $row['alkhdmatfsl_bynhm_bfsl'],
-                            'cost' => $row['costs_separate_with_comma'],
-                            'costArabic' => $row['saar_alkhdmat_fsl_bynhm_bfsl'],
-                            'state' => strtolower($row['availablemaintenance']),
-                            'description' => $row['description'],
-                            'ArabicDescription' => $row['arabicdescription'],
-                            'price'=>$row['device_price'],
-                            'AdditionalInfo' => $row['additionalinfo'],
-                            'ArabicAddInfo' => $row['arabicaddinfo'],
-                            'ManufactureYear' => $row['manufactureyear'],
-                            'MaintenanceContract' => $row['maintenancecontract'],
-                            'ManufactureCountry' => $row['manufacturecountry'],
-                            'ManufactureWebsite' => $row['manufacturewebsite'],
-                            'entry_date' => date('Y-m-d H:i:s')])
-                        :
-                        devices::create([
-                            'name' => $row['name'],
-                            'Arabicname' => $row['arabic_name'],
-                            'ImagePath' => 'images/universities/' . auth()->user()->uni_id . '/' . $row['image_name_with_extension'],
-                            'model' => $row['model'],
-                            'manufacturer' => $row['manufacturer'],
-                            'lab_id' => $row['department_name'] == null ? labs::where('name', $row['lab_name'])->where('fac_id', $this->fac_id)->Where(function($query) {
-                                $query->WhereNull('dept_id')
-                                    ->orwhere('dept_id', 0);
-                            })->pluck('id')->first()
-                                : labs::where('name', $row['lab_name'])->where('fac_id', $this->fac_id)->where('dept_id', $dept_ids[$row['department_name']])->pluck('id')->first(),
-                            'num_units' => $row['num_units'],
-                            'services' => $row['services_separate_with_comma'],
-                            'servicesArabic' => $row['alkhdmatfsl_bynhm_bfsl'],
-                            'cost' => $row['costs_separate_with_comma'],
-                            'costArabic' => $row['saar_alkhdmat_fsl_bynhm_bfsl'],
-                            'state' => $row['availablemaintenance'],
-                            'description' => $row['description'],
-                            'ArabicDescription' => $row['arabicdescription'],
-                            'price'=>$row['device_price'],
-                            'AdditionalInfo' => $row['additionalinfo'],
-                            'ArabicAddInfo' => $row['arabicaddinfo'],
-                            'ManufactureYear' => $row['manufactureyear'],
-                            'MaintenanceContract' => $row['maintenancecontract'],
-                            'ManufactureCountry' => $row['manufacturecountry'],
-                            'ManufactureWebsite' => $row['manufacturewebsite'],
-                            'entry_date' => date('Y-m-d H:i:s')]);
-                }
-                $cost = explode(',',$row['costs_separate_with_comma']);
-                $services = explode(',',$row['services_separate_with_comma']);
-                $costArabic = explode(',',$row['saar_alkhdmat_fsl_bynhm_bfsl']);
-                $servicesArabic = explode(',',$row['alkhdmatfsl_bynhm_bfsl']);
 
-                //if service with no cost value
-                while(count($services) > count($cost)) {    array_push($cost,'');   }
-                //if Arabic service with no cost value
-                while(count($servicesArabic) > count($costArabic)) {    array_push($costArabic,''); }
-                //if service with no arabic equivalent
-                while(count($services) > count($servicesArabic)){
-                    array_push($servicesArabic,'');
-                    array_push($costArabic,'');
-                }
-                foreach ($services as $key=>$value){
-                    $service = $this->fac_id =='central'?
-                        services::create(['device_id'=>$device->id, 'service_name'=>$value, 'cost'=>$cost[$key],
-                            'service_arabic'=>$servicesArabic[$key], 'cost_arabic'=>$costArabic[$key], 'central'=>1])
-                        :
-                        services::create(['device_id'=>$device->id, 'service_name'=>$value, 'cost'=>$cost[$key],
-                            'service_arabic'=>$servicesArabic[$key], 'cost_arabic'=>$costArabic[$key], 'central'=>0]);
+        try {
+            // get department IDs
+            if ($this->fac_id != 'central') {
+                if (Auth()->user()->hasRole('university')) {
+                    $dept_ids = dept_fac::where('uni_id', Auth()->user()->uni_id)
+                        ->where('fac_id', $this->fac_id)
+                        ->join('departments', 'dept_fac.dept_id', '=', 'departments.id')
+                        ->pluck('departments.id', 'departments.name');
+                } else {
+                    $dept_ids = dept_fac::where('uni_id', Auth()->user()->uni_id)
+                        ->where('fac_id', Auth()->user()->fac_id)
+                        ->join('departments', 'dept_fac.dept_id', '=', 'departments.id')
+                        ->pluck('departments.id', 'departments.name');
                 }
             }
-        } catch(\Exception $e) {
+
+            foreach ($rows as $row) {
+                if ($row->filter()->isEmpty()) continue;
+
+                // Get lab_id safely
+                if ($this->fac_id == 'central') {
+                    $lab_id = UniLabs::where('name', $row['lab_name'])
+                        ->where('uni_id', auth()->user()->uni_id)
+                        ->value('id');
+                } else {
+                    $dept_id = isset($dept_ids[$row['department_name']]) ? $dept_ids[$row['department_name']] : null;
+
+                    $labQuery = labs::where('name', $row['lab_name'])
+                        ->where('fac_id', $this->fac_id);
+
+                    if ($dept_id) {
+                        $labQuery->where('dept_id', $dept_id);
+                    } else {
+                        $labQuery->where(function ($query) {
+                            $query->whereNull('dept_id')->orWhere('dept_id', 0);
+                        });
+                    }
+
+                    $lab_id = $labQuery->value('id');
+                }
+
+                // Build common data array
+                $data = [
+                    'name' => $row['name'],
+                    'Arabicname' => $row['arabic_name'],
+                    'ImagePath' => 'images/universities/' . auth()->user()->uni_id . '/' . $row['image_name_with_extension'],
+                    'model' => $row['model'],
+                    'manufacturer' => $row['manufacturer'],
+                    'lab_id' => $lab_id,
+                    'num_units' => $row['num_units'],
+                    'services' => $row['services_separate_with_comma'],
+                    'servicesArabic' => $row['alkhdmatfsl_bynhm_bfsl'],
+                    'cost' => $row['costs_separate_with_comma'],
+                    'costArabic' => $row['saar_alkhdmat_fsl_bynhm_bfsl'],
+                    'state' => strtolower($row['availablemaintenance']),
+                    'description' => $row['description'],
+                    'ArabicDescription' => $row['arabicdescription'],
+                    'price' => $row['device_price'],
+                    'AdditionalInfo' => $row['additionalinfo'],
+                    'ArabicAddInfo' => $row['arabicaddinfo'],
+                    'ManufactureYear' => $row['manufactureyear'],
+                    'MaintenanceContract' => $row['maintenancecontract'],
+                    'ManufactureCountry' => $row['manufacturecountry'],
+                    'ManufactureWebsite' => $row['manufacturewebsite'],
+                    'entry_date' => now(),
+                ];
+
+                // Check if device exists (by name + model + lab)
+                if ($this->fac_id == 'central') {
+                    $device = UniDevices::where('uni_id', auth()->user()->uni_id)
+                        ->where('name', $row['name'])
+                        ->where('model', $row['model'])
+                        ->where('lab_id', $lab_id)
+                        ->first();
+                } else {
+                    $device = devices::where('name', $row['name'])
+                        ->where('model', $row['model'])
+                        ->where('lab_id', $lab_id)
+                        ->first();
+                }
+
+                // Update or create
+                if ($device) {
+                    $device->update($data);
+                } else {
+                    if ($this->fac_id == 'central') {
+                        $data['uni_id'] = auth()->user()->uni_id;
+                        $device = UniDevices::create($data);
+                    } else {
+                        $device = devices::create($data);
+                    }
+                }
+
+                // --- Sync Services ---
+                $cost = explode(',', $row['costs_separate_with_comma']);
+                $servicesList = explode(',', $row['services_separate_with_comma']);
+                $costArabic = explode(',', $row['saar_alkhdmat_fsl_bynhm_bfsl']);
+                $servicesArabic = explode(',', $row['alkhdmatfsl_bynhm_bfsl']);
+
+                // normalize array lengths
+                while (count($servicesList) > count($cost)) array_push($cost, '');
+                while (count($servicesArabic) > count($costArabic)) array_push($costArabic, '');
+                while (count($servicesList) > count($servicesArabic)) {
+                    array_push($servicesArabic, '');
+                    array_push($costArabic, '');
+                }
+
+                // delete old services before re-adding
+                services::where('device_id', $device->id)->delete();
+
+                foreach ($servicesList as $key => $value) {
+                    if (trim($value) != '') {
+                        services::create([
+                            'device_id' => $device->id,
+                            'service_name' => $value,
+                            'cost' => $cost[$key],
+                            'service_arabic' => $servicesArabic[$key],
+                            'cost_arabic' => $costArabic[$key],
+                            'central' => $this->fac_id == 'central' ? 1 : 0,
+                        ]);
+                    }
+                }
+            }
+        } catch (\Exception $e) {
             throw $e;
         }
     }
